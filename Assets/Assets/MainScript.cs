c16591d01135cbb1840cb62cbbbf54f0ea0a3546
Vishnu <vishnumnn4@gmail.com>
2019-10-15 11:02:47 -0500

    Changed how validity of a randomly generated sphere is found

--- Assets/Assets/MainScript.cs
+++ Assets/Assets/MainScript.cs
@@ -7,8 +7,8 @@ public class MainScript : MonoBehaviour
 {
     // Final members
     private const int NUM_RETRIES = 60;
-    private const float X_RANGE = 30.0f;
-    private const float Z_RANGE = 30.0f;
+    private const float X_RANGE = 38.0f;
+    private const float Z_RANGE = 38.0f;
 
     // Unity editor set variables
     public ComputeShader RayTracerShader;
@@ -17,7 +17,7 @@ public class MainScript : MonoBehaviour
     public Light Light;
 
     // Unity editor set slider variables
-    [Range(1, 25)]
+    [Range(1, 34)]
     public int numSpheres = 2;
     [Range(0.25f, 2.0f)]
     public float sphereRadiusMin = 1.0f;
@@ -37,61 +37,57 @@ public class MainScript : MonoBehaviour
         public Vector4 color;
         public Vector4 specular;
     }
-    private bool isValid(Sphere s, Vector4 vec)
+    private bool isValid(Sphere s, List<Sphere> created)
     {
-        if (s.origin.x >= vec.x && s.origin.x <= vec.y && s.origin.z >= vec.z && s.origin.z <= vec.w)
+        foreach(Sphere e in created)
         {
-            return false;
+            if (Mathf.Sqrt(Mathf.Pow(s.origin.x - e.origin.x, 2) + Mathf.Pow(s.origin.z - e.origin.z, 2)) < (s.radius + e.radius))
+            {
+                return false;
+            }
         }
         return true;
     }
-    // This is a rough sketch. Checking validity can be modified to take log(x+y) time. Would need to create balanced tree though.
-    // Maybe update to simple distance checking for n time validity checking.
+    
+    /// <summary>
+    /// Randomly generate different types of spheres to be passed to the compute shader.
+    /// </summary>
     private void GetSpheres()
     {
-        Vector4[] occupiedRegions = new Vector4[numSpheres];
-        List<Sphere> spheres = new List<Sphere>();
+        List<Sphere> created = new List<Sphere>();
         for (int i = 0; i < numSpheres; i++)
         {
-            Debug.Log(numSpheres);
-            Debug.Log(NUM_RETRIES);
-            bool success = true;
             int numTries = 0;
+
+            // Bounds for origin generation in x-z plane
+            float maxx = -sphereRadiusMax + 0.5f * X_RANGE;
+            float minx = -0.5f * X_RANGE + sphereRadiusMax;
+            float maxz = Z_RANGE - sphereRadiusMax;
+            float minz = sphereRadiusMax;
+
+            // Random sphere feature generation
             Sphere sphere;
-            sphere.radius = Random.Range(sphereRadiusMin, sphereRadiusMax);
             sphere.origin = new Vector3(-1.0f, -1.0f, -1.0f);
             sphere.color = new Vector4(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f);
             sphere.specular = Random.Range(0.0f,1.0f) > 0.5f ? new Vector4(0.0f,0.0f,0.0f,1.0f) : new Vector4(0.6f, 0.6f, 0.6f, 1.0f);
+
+            // Keep trying to add spheres with random origin values while numTries is not too high.
             while (numTries < NUM_RETRIES)
             {
-                float maxx = X_RANGE + sphereRadiusMax;
-                float maxz = Z_RANGE + sphereRadiusMax;
-                sphere.origin = new Vector3(Random.Range(sphereRadiusMax, maxx), sphere.radius, Random.Range(sphereRadiusMax, maxz));
+                sphere.radius = Random.Range(sphereRadiusMin, sphereRadiusMax);
+                sphere.origin = new Vector3(Random.Range(minx, maxx), sphere.radius, Random.Range(minz, maxz));
                 //Debug.Log(sphere.origin);
-                foreach (Vector4 vec in occupiedRegions)
+                if (isValid(sphere, created))
                 {
-                    if (vec != null && !isValid(sphere, vec))
-                    {
-                        success = false;
-                        numTries++;
-                        break;
-                    }
-                }
-                if (success)
-                {
-                    Debug.Log("Added Successfully");
-                    occupiedRegions[i].x = sphere.origin.x - sphere.radius * 2;
-                    occupiedRegions[i].y = sphere.origin.x + sphere.radius * 2;
-                    occupiedRegions[i].z = sphere.origin.z - sphere.radius * 2;
-                    occupiedRegions[i].w = sphere.origin.z + sphere.radius * 2;
-                    spheres.Add(sphere);
+                    created.Add(sphere);
+                    numTries = 0;
                     break;
                 }
+                numTries++;
             }
         }
-        Debug.Log(occupiedRegions);
-        this.spheres = new ComputeBuffer(spheres.Count, 48);
-        this.spheres.SetData(spheres);
+        this.spheres = new ComputeBuffer(created.Count, 48);
+        this.spheres.SetData(created);
     }
 
     private void Awake()
